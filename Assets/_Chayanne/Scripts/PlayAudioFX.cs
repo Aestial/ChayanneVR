bef71e4c62cc50d5e95edd77b01f70c78420c29d
Jaime Hernandez <jaimevazz@gmail.com>
2020-04-20 08:25:44 -0500

    Added Chayanne project files

---
+++ Assets/_Chayanne/Scripts/PlayAudioFX.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayAudioFX : MonoBehaviour
+{
+    [Serializable]
+    public struct AudioFX
+    {
+        public AudioClip clip;
+        public float volume;
+        public float pitch;
+    }
+
+    [SerializeField] AudioFX fX = default; 
+    AudioSource audioSource;
+
+    public void PlayFX()
+    {
+        PlayOneShot(fX);
+    }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();        
+    }
+    void PlayOneShot(AudioFX fx)
+    {
+        float oldPitch = audioSource.pitch;
+        audioSource.pitch = fx.pitch;
+        audioSource.PlayOneShot(fx.clip, fx.volume);
+        audioSource.pitch = oldPitch;
+    }
+}
---
+++ Assets/_Chayanne/Scripts/ShotGrabbed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGrabbed : MonoBehaviour
+{
+    public OVRInput.Button actionButton;
+    OVRGrabbable ovrGrabbable;
+    Animator animator;      
+
+    void Start()
+    {        
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (ovrGrabbable.isGrabbed &&
+            OVRInput.GetDown(actionButton, ovrGrabbable.grabbedBy.GetController()))
+        {
+            animator.SetTrigger("Shoot");
+        }
+    }
+}
