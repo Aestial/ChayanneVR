ca8709354ab57062fb045a4e2daebad5a70c8355
Jaime Hernandez <jaimevazz@gmail.com>
2020-04-25 00:02:32 -0500

    Added GrabbableRespawner

---
+++ Assets/_Chayanne/Scripts/GrabbableRespawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableRespawner : MonoBehaviour
+{
+    [SerializeField] float minfloorHeight;
+    Transform original;
+
+    void Start()
+    {        
+        original.position = transform.position;
+        original.rotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (transform.position.y < minfloorHeight)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        transform.position = original.position;
+        transform.rotation = original.rotation;
+    }
+}
