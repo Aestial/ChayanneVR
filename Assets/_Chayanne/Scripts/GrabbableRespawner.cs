572386b1b66c821bf6ecb368972f0220319f4983
Jaime Hernandez <jaimevazz@gmail.com>
2020-05-16 15:28:49 -0500

    Respawn from floor behaviour on grabbables

--- Assets/_Chayanne/Scripts/GrabbableRespawner.cs
+++ Assets/_Chayanne/Scripts/GrabbableRespawner.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GrabbableRespawner : MonoBehaviour
 {
     [SerializeField] float minfloorHeight = 0.0f;
+    [SerializeField] float waitTime = 3.0f;
     Vector3 originalPos = new Vector3();
     Quaternion originalRot = new Quaternion();
     new Rigidbody rigidbody;
+    bool inFloor;
 
     void Start()
     {
@@ -18,15 +21,30 @@ public class GrabbableRespawner : MonoBehaviour
     {
         if (transform.position.y < minfloorHeight)
         {
-            Respawn();
+            if (!inFloor)
+                WaitAndRespawn(waitTime);
+            inFloor = true;
         }
     }
 
-    private void Respawn()
+    void WaitAndRespawn(float seconds)
     {
+        StartCoroutine(WaitAndRespawnCoroutine(seconds));
+    }
+
+    void Respawn()
+    {        
         transform.position = originalPos;
         transform.rotation = originalRot;
         rigidbody.velocity = Vector3.zero;
         rigidbody.angularVelocity = Vector3.zero;
+        inFloor = false;
     }
+
+    IEnumerator WaitAndRespawnCoroutine(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Respawn();
+    }
+
 }
